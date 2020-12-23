11728e2de6cc05d6df191cc93a979fb010b2684a
Rebecca J Proctor <jproctor.m.ed.tn@gmail.com>
2020-12-23 21:02:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# fundamental data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# data typles are values in python that store information and modify it in python
+
+# classes -> create your own custom types
+
+# specialized data types -> packages or modules from libraries house these and can be imported as extentions
+
+# None type -> nothing/the absence of value
+
